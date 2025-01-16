d837a141eb04373ff84478ca70538617a8872949
William Cui <williamcui888@hotmail.com>
2025-01-15 20:00:19 -0800

    Enabling CORS on Fastify for deployment.

--- take-home-frontend/src/CreateQueryModal.tsx
+++ take-home-frontend/src/CreateQueryModal.tsx
@@ -2,11 +2,9 @@ import { Button } from "@/components/ui/button";
 import {
   Dialog,
   DialogContent,
-  DialogDescription,
   DialogFooter,
   DialogHeader,
   DialogTitle,
-  DialogTrigger,
 } from "@/components/ui/dialog";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
@@ -16,7 +14,7 @@ import {
   TooltipProvider,
   TooltipTrigger,
 } from "@/components/ui/tooltip";
-import { Plus, Check, CircleHelp } from "lucide-react";
+import { Plus } from "lucide-react";
 import { useState } from "react";
 import axios from "./http-common";
 import { useMutation } from "react-query";
--- take-home-frontend/src/ViewQueryModal.tsx
+++ take-home-frontend/src/ViewQueryModal.tsx
@@ -2,13 +2,10 @@ import { Button } from "@/components/ui/button";
 import {
   Dialog,
   DialogContent,
-  DialogDescription,
   DialogFooter,
   DialogHeader,
   DialogTitle,
-  DialogTrigger,
 } from "@/components/ui/dialog";
-import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import {
   Tooltip,
