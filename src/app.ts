1622e00de7145a18a298007d0ca9385a2a00b0ef
William Cui <williamcui888@hotmail.com>
2025-01-15 20:14:02 -0800

    Removing CORS from app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,4 @@
 import fastify from 'fastify'
-import cors from '@fastify/cors'
 
 import formDataRoutes from './routes/form_data'
 import queryRoutes from './routes/query'
@@ -7,10 +6,6 @@ import errorHandler from './errors'
 
 function build(opts = {}) {
   const app = fastify(opts)
-
-  app.register(cors, {
-    origin: 'https://playful-truffle-af381c.netlify.app',
-  })
 
   app.register(formDataRoutes, { prefix: '/form-data' })
   app.register(queryRoutes, { prefix: '/query' })
